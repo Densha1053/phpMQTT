79ea502f6da0211ae764d5a433270fc55876fe28
Densha1053 <a2p2i2t1@icloud.com>
2017-06-12 19:13:04 +0700

    Update linechatbot.php

--- linechatbot.php
+++ linechatbot.php
@@ -15,7 +15,7 @@ $mqtt = new phpMQTT($host, $port, "ClientID".rand());
 $token = "c//eUJe6lMKtCicCrC9eCSE5pHZvRiCgavKE5bI6Jd8ujPcvCubtGWhUloHHixBOumFO6IRkKD+q9+AYcU/0tcylBJcaZpWUhotRTPJbQpLkjbzjjl8Q1UwTw60olaqh0fRR7qi3AEYzFej6zDDoyQdB04t89/1O/w1cDnyilFU="; //นำ token ที่มาจาก line developer account ของเรามาใส่ครับ
 
 $httpClient = new CurlHTTPClient($token);
-$bot = new LINEBot($httpClient, [‘channelSecret’ => $token]);
+$bot = new LINEBot($httpClient, [‘0fcee9d249316119f6d98b361a420b90’ => $token]);
 // webhook
 $jsonStr = file_get_contents(‘php://input’);
 $jsonObj = json_decode($jsonStr);
