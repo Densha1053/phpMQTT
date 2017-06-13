7eca6157ec815e5afc288634a1ec242b14059fbc
Densha1053 <a2p2i2t1@icloud.com>
2017-06-13 15:28:16 +0700

    Create bot.php

--- bot.php
+++ bot.php
@@ -17,49 +17,58 @@ $jsonObj = json_decode($jsonStr);
 print_r($jsonStr);
                              
 foreach ($jsonObj->events as $event) {
-if(‘message’ == $event->type){
-debug
-file_put_contents(“message.json”, json_encode($event));
-$text = $event->message->text;
+	if(‘message’ == $event->type){
+		debug
+		file_put_contents(“message.json”, json_encode($event));
+		$text = $event['message']['text'];
+		$replyToken = $event['replyToken'];
 
-if (preg_match(“/สวัสดี/”, $text)) {
-$text = “มีอะไรให้Denshaรับใช้ครับ”;
-}
+		if (preg_match(“/สวัสดี/”, $text)) {
+			$text = “มีอะไรให้Denshaรับใช้ครับ”;
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+		}
 
-if (preg_match(“/เปิดทีวี/”, $text)) {     //หากในแชตที่ส่งมามีคำว่า เปิดทีวี ก็ให้ส่ง mqtt ไปแจ้ง server เราครับ
-if ($mqtt->connect(true,NULL,$username,$password)) {
-$mqtt->publish(“/Benz1053/room1”,”TV”,0); // ตัวอย่างคำสั่งเปิดทีวีที่จะส่งไปยัง mqtt server
-$mqtt->close();
-}
-$text = “เปิดทีวีให้แล้วคร้าบบบบ”;
-}
-if (preg_match(“/ปิดทีวี/”, $text) and !preg_match(“/เปิดทีวี/”, $text)) {
-if ($mqtt->connect(true,NULL,$username,$password)) {
-$mqtt->publish(“/Benz1053/room1”,”TV”,0);
-$mqtt->close();
-}
-$text = “จ่าปิดทีวีให้แล้วนะครับ!!”;
-}
-$url = 'https://api.line.me/v2/bot/message/reply';
-$data = [
-  'replyToken' => $replyToken,
-	'messages' => [$text],
-];
-$post = json_encode($data);
-$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $token);
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
+		if (preg_match(“/เปิดทีวี/”, $text)) {     //หากในแชตที่ส่งมามีคำว่า เปิดทีวี ก็ให้ส่ง mqtt ไปแจ้ง server เราครับ
+			if ($mqtt->connect(true,NULL,$username,$password)) {
+				$mqtt->publish(“/Benz1053/room1”,”TV”,0); // ตัวอย่างคำสั่งเปิดทีวีที่จะส่งไปยัง mqtt server
+				$mqtt->close();
+			}
+			$text = “เปิดทีวีให้แล้วคร้าบบบบ”;
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+		}
+		if (preg_match(“/ปิดทีวี/”, $text) and !preg_match(“/เปิดทีวี/”, $text)) {
+			if ($mqtt->connect(true,NULL,$username,$password)) {
+				$mqtt->publish(“/Benz1053/room1”,”TV”,0);
+				$mqtt->close();
+			}
+			$text = "ปิดทีวีให้แล้วนะครับ!!”;
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+		}
+		$url = 'https://api.line.me/v2/bot/message/reply';
+		$data = [
+			'replyToken' => $replyToken,
+			'messages' => [$messages],
+		];
+		$post = json_encode($data);
+		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $token);
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		$result = curl_exec($ch);
+		curl_close($ch);
 
-echo $result . "\r\n";
-  
-
+		echo $result . "\r\n";  
+	}
 }
-}
-
-?>
