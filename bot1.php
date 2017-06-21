6e39f2d5b45b2d978c72b4dc3d884683ed65a35e
Densha1053 <a2p2i2t1@icloud.com>
2017-06-21 19:50:15 +0700

    Update bot1.php

--- bot1.php
+++ bot1.php
@@ -348,14 +348,11 @@ if (!is_null($events['events'])) {
 //currently subscribed topics
 			$topics['/Benz1053/room1'] = array("qos"=>0, "function"=>"procmsg");
 			$mqtt->subscribe($topics,0);
-
-		
+			$text = $msg;
 
 			$mqtt->close();
 			function procmsg($topic,$msg){
   				echo "Msg Recieved: $msg";
-				$text = $msg;
-				return $text;
 			}
 			
 			$messages = [
