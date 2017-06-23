8f1accfe09d22ba4e0c9f67c062681eb5606248d
Densha1053 <a2p2i2t1@icloud.com>
2017-06-23 13:26:37 +0700

    Update bot1.php

--- bot1.php
+++ bot1.php
@@ -383,26 +383,8 @@ if (!is_null($events['events'])) {
   			}
 			$text = "time is";
 			$messages = [
-				{
- 				"type": "template",
-  				"altText": "this is a confirm template",
-  				"template": {
-      					"type": "confirm",
-      					"text": "Are you sure?",
-      					"actions": [
-          					{
-            					"type": "message",
-            					"label": "Yes",
-            					"text": "yes"
-          					},
-          					{
-            					"type": "message",
-            					"label": "No",
-            					"text": "no"
-          					}
-      					]
-  				}
-				}
+				"type": "text",
+    				"text": $text
 			];
 
 
